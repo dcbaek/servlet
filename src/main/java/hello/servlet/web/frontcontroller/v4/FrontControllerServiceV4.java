8124d66c8efe6ef211bd2afdc3f5bf5e23e68897
dcbaek <ehdco8988@gmail.com>
2022-08-22 13:31:07 +0900

    ControllerV5

--- src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServiceV4.java
+++ src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServiceV4.java
@@ -1,11 +1,6 @@
 package hello.servlet.web.frontcontroller.v4;
 
-import hello.servlet.web.frontcontroller.ModelView;
 import hello.servlet.web.frontcontroller.MyView;
-import hello.servlet.web.frontcontroller.v3.ControllerV3;
-import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
-import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
-import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
 import hello.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
 import hello.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
 import hello.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
@@ -56,6 +51,7 @@ public class FrontControllerServiceV4 extends HttpServlet {
     }
 
     private MyView viewResolver(String viewName) {
+
         return new MyView("/WEB-INF/views/" + viewName + ".jsp");
     }
 
